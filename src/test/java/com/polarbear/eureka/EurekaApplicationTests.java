6b57b8bd733e0a9f6da8bc504076a9248a2ca19e
git-kkumar <77931687+git-kkumar@users.noreply.github.com>
2024-11-26 23:53:46 +0530

    Initial commit

---
+++ src/test/java/com/polarbear/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbear.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
